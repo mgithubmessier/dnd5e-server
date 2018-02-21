6de189365fcebfcecec0b524f967165342ea41ba
Matthew Messier <messier_matthew@yahoo.com>
2018-02-20 23:56:41 -0500

    only using one db connection, and the _id parameter was getting attached to the save post request before...solved that after forever

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-var MongoClient = require('mongodb').MongoClient;
+var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
 
 // setup
@@ -9,6 +9,9 @@ var app = express();
 // constants
 var dbURL = 'mongodb://localhost:27017';
 var dbName = 'dnd5eDB';
+
+var mongoConnection;
+
 
 var corsOptions = {
   origin: 'http://localhost:4200',
@@ -23,44 +26,41 @@ app.use(function(req, res, next) {
   next();
 });
 
+function getMongoConnection() {
+  if(!mongoConnection) {
+    mongoConnection = mongodb.connect(dbURL);
+  } 
+  return mongoConnection;
+}
+
 app.post('/save', function (req, res, next) {
-  MongoClient.connect(dbURL, function(error, client) {
-    if(error) {
-      console.error('SAVE Unable to connect to server');
-    } else {
-      console.log('SAVE connection established');
+  getMongoConnection().then(function(client) {
       var db = client.db(dbName);
-
-      db.collection('abilities').update({ "name":req.body.name }, req.body,  function(error, result) {
-        console.log('SAVE RESULT: ', result);
-        res.header("Access-Control-Allow-Headers", "http://localhost:4200")
-        res.json({
-          msg: result
-        });
-        client.close();    
+      if(req.body._id) {
+        delete req.body._id;
+      }
+      db.collection('abilities').update({ "name":req.body.name }, req.body, function(error, result) {
+        if(error) {
+          console.error(error);
+        } else {
+          res.json({ msg: result });
+        }
       });
-
-    }
   });
 });
 
 app.get('/get', function (req, res, next) {
-  MongoClient.connect(dbURL, function(error, client) {
-    if(error) {
-      client.close();    
-      console.error('GET Unable to connect to server');
-    } else {
-      console.log('GET connection established');
+  getMongoConnection().then(function(client) {
       var db = client.db(dbName);
       db.collection('abilities').find({}).toArray(function(error, result) {
-        res.json(result);
-        client.close();    
+        if(error) {
+          console.error(error);
+        } else {
+          res.json(result);
+        }
       });
-    }
   });
 });
-
-
 
 app.post('/login', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for only example.com.'})
